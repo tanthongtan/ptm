6525b5180cdb0b137afb6bc5f66016ecf8896a4e
tanthongtan <tan.thongtan1@gmail.com>
2020-02-23 14:11:06 +0700

    fixed distributions over theta/pi

--- distributions.py
+++ distributions.py
@@ -2,6 +2,13 @@ import torch
 from lcdk import Logcdk
 import torch.nn.functional as F
 
+def unnormalized_log_prob_spherical_dirichlet(alpha, pi):
+    return ((2*alpha-1)*torch.log(torch.abs(pi))).sum(dim=-1)
+
+def log_prob_von_mises_fisher(D, natural_param, X):
+    logcdk = Logcdk.apply
+    return logcdk(D, natural_param.norm(p=2, dim=-1)) + (natural_param * X).sum(dim = -1)
+
 class SamFullConditionalPiDistribution:
     
     def __init__(self, x, mu, alpha, kappa1):
@@ -11,20 +18,18 @@ class SamFullConditionalPiDistribution:
         self.kappa1 = kappa1
         self.independent_axes = 1
 
-    def unnormalized_log_prob_spherical_dirichlet(self, pi):
-        return ((2*self.alpha-1)*torch.log(torch.abs(pi))).sum(dim=-1)
-    
-    def unnormalized_log_prob(self, pi):
+    def unnormalized_log_prob(self, theta):
+        pi = theta**2
         avg = F.normalize(torch.matmul(pi,self.mu), p=2, dim=-1)
-        return self.unnormalized_log_prob_spherical_dirichlet(pi) \
+        return unnormalized_log_prob_spherical_dirichlet(self.alpha, theta) \
                 + (self.kappa1 * avg * self.x).sum(dim=-1)
             
 
 class SamFullConditionalMuDistribution:
     
-    def __init__(self, x, pi, c0, mu0, kappa0, kappa1):
+    def __init__(self, x, theta, c0, mu0, kappa0, kappa1):
         self.x = x
-        self.pi = pi
+        self.theta = theta
         self.c0 = c0
         self.mu0 = mu0
         self.kappa0 = kappa0
@@ -33,6 +38,7 @@ class SamFullConditionalMuDistribution:
     
     def unnormalized_log_prob(self, mu):
         logcdk = Logcdk.apply
-        avg = F.normalize(torch.matmul(self.pi,mu), p=2, dim=-1)
+        pi = self.theta ** 2
+        avg = F.normalize(torch.matmul(pi,mu), p=2, dim=-1)
         return (self.kappa1 * avg * self.x).sum(dim=-1).sum() \
                 - logcdk(self.mu0.shape[-1], (self.kappa0 * self.mu0 + self.c0 * mu.sum(dim=0)).norm(p=2, dim=-1))
