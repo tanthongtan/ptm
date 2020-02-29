0ec0ab2c6c2d3febee7b53fda30ec8e2264d92cc
tanthongtan <tan.thongtan1@gmail.com>
2020-02-29 15:41:53 +0700

    fix vmf sample

--- vmf_sample.py
+++ vmf_sample.py
@@ -2,8 +2,6 @@ import math
 import torch
 from torch.distributions.kl import register_kl
 
-from hyperspherical_vae.ops.ive import ive
-from hyperspherical_vae.distributions.hyperspherical_uniform import HypersphericalUniform
 
 import numpy as np
 import scipy.special
@@ -47,6 +45,48 @@ class Ive(torch.nn.Module):
         return ive(self.v, z)
 
 ive = IveFunction.apply
+
+class HypersphericalUniform(torch.distributions.Distribution):
+
+    support = torch.distributions.constraints.real
+    has_rsample = False
+    _mean_carrier_measure = 0
+
+    @property
+    def dim(self):
+        return self._dim
+
+    @property
+    def device(self):
+        return self._device
+
+    @device.setter
+    def device(self, val):
+        self._device = val if isinstance(val, torch.device) else torch.device(val)
+
+    def __init__(self, dim, validate_args=None, device="cpu"):
+        super(HypersphericalUniform, self).__init__(torch.Size([dim]), validate_args=validate_args)
+        self._dim = dim
+        self.device = device
+
+    def sample(self, shape=torch.Size()):
+        output = torch.distributions.Normal(0, 1).sample(
+            (shape if isinstance(shape, torch.Size) else torch.Size([shape])) + torch.Size([self._dim + 1])).to(self.device)
+
+        return output / output.norm(dim=-1, keepdim=True)
+
+    def entropy(self):
+        return self.__log_surface_area()
+
+    def log_prob(self, x):
+        return - torch.ones(x.shape[:-1], device=self.device) * self.__log_surface_area()
+
+    def __log_surface_area(self):
+        if torch.__version__ >= "1.0.0":
+            lgamma = torch.lgamma(torch.tensor([(self._dim + 1) / 2]).to(self.device))
+        else:
+            lgamma = torch.lgamma(torch.Tensor([(self._dim + 1) / 2], device=self.device))
+        return math.log(2) + ((self._dim + 1) / 2) * math.log(math.pi) - lgamma
 
 class VonMisesFisher(torch.distributions.Distribution):
 
@@ -173,44 +213,3 @@ def _kl_vmf_uniform(vmf, hyu):
     return - vmf.entropy() + hyu.entropy()
 
 
-class HypersphericalUniform(torch.distributions.Distribution):
-
-    support = torch.distributions.constraints.real
-    has_rsample = False
-    _mean_carrier_measure = 0
-
-    @property
-    def dim(self):
-        return self._dim
-
-    @property
-    def device(self):
-        return self._device
-
-    @device.setter
-    def device(self, val):
-        self._device = val if isinstance(val, torch.device) else torch.device(val)
-
-    def __init__(self, dim, validate_args=None, device="cpu"):
-        super(HypersphericalUniform, self).__init__(torch.Size([dim]), validate_args=validate_args)
-        self._dim = dim
-        self.device = device
-
-    def sample(self, shape=torch.Size()):
-        output = torch.distributions.Normal(0, 1).sample(
-            (shape if isinstance(shape, torch.Size) else torch.Size([shape])) + torch.Size([self._dim + 1])).to(self.device)
-
-        return output / output.norm(dim=-1, keepdim=True)
-
-    def entropy(self):
-        return self.__log_surface_area()
-
-    def log_prob(self, x):
-        return - torch.ones(x.shape[:-1], device=self.device) * self.__log_surface_area()
-
-    def __log_surface_area(self):
-        if torch.__version__ >= "1.0.0":
-            lgamma = torch.lgamma(torch.tensor([(self._dim + 1) / 2]).to(self.device))
-        else:
-            lgamma = torch.lgamma(torch.Tensor([(self._dim + 1) / 2], device=self.device))
-        return math.log(2) + ((self._dim + 1) / 2) * math.log(math.pi) - lgamma
