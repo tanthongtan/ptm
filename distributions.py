5f9837c8be7002b693009c05dbdf96e4f92c0b57
tanthongtan <tan.thongtan1@gmail.com>
2020-05-16 18:44:17 +0700

    added more spheredir dists

--- distributions.py
+++ distributions.py
@@ -67,6 +67,26 @@ class SamJointDistributionWithStickDir:
         return (self.kappa1 * avg * self.x).sum(dim=-1).sum() \
                 - logcdk(self.mu0.shape[-1], (self.kappa0 * self.mu0 + self.c0 * mu.sum(dim=0)).norm(p=2, dim=-1)) \
                 + log_prob_stickbreaking_dirichlet(self.alpha, theta, pi).sum()
+                
+class SamJointDistributionWithSphereDir:
+    
+    def __init__(self, x, alpha, c0, mu0, kappa0, kappa1):
+        self.x = x
+        self.alpha = alpha
+        self.c0 = c0
+        self.mu0 = mu0
+        self.kappa0 = kappa0
+        self.kappa1 = kappa1
+        
+    def unnormalized_log_prob(self, params):
+        logcdk = Logcdk.apply
+        theta = params['theta']
+        pi = theta ** 2.
+        mu = params['mu']
+        avg = F.normalize(torch.matmul(pi,mu), p=2, dim=-1)
+        return (self.kappa1 * avg * self.x).sum(dim=-1).sum() \
+                - logcdk(self.mu0.shape[-1], (self.kappa0 * self.mu0 + self.c0 * mu.sum(dim=0)).norm(p=2, dim=-1)) \
+                + unnormalized_log_prob_spherical_dirichlet(self.alpha, theta).sum()
 
 class VptmFullConditionalThetaDistribution:
     
@@ -244,6 +264,23 @@ class MptmJointDistributionWithStickDir:
                 + log_prob_stickbreaking_dirichlet(self.alpha, theta, pi).sum() \
                 + log_prob_stickbreaking_dirichlet(self.eta, lamb, beta).sum()
                 
+class MptmJointDistributionWithSphereDir:
+    
+    def __init__(self, x, alpha, eta):
+        self.x = x 
+        self.alpha = alpha
+        self.eta = eta
+        
+    def unnormalized_log_prob(self, params):
+        theta = params['theta']
+        pi = theta ** 2.
+        lamb = params['lamb']
+        beta = torch.distributions.StickBreakingTransform()(lamb)
+        avg = torch.exp(torch.matmul(pi, torch.log(beta)))
+        return dist.Multinomial(probs = avg).log_prob(self.x).sum() \
+                + unnormalized_log_prob_spherical_dirichlet(self.alpha, theta).sum() \
+                + unnormalized_log_prob_spherical_dirichlet(self.eta, lamb).sum()
+                
 class LdaJointDistributionWithStickDir:
     
     def __init__(self, x, alpha, eta):
@@ -259,5 +296,22 @@ class LdaJointDistributionWithStickDir:
         avg = torch.matmul(pi, beta)
         return dist.Multinomial(probs = avg).log_prob(self.x).sum() \
                 + log_prob_stickbreaking_dirichlet(self.alpha, theta, pi).sum() \
-                + log_prob_stickbreaking_dirichlet(self.eta, lamb, beta).sum()                
+                + log_prob_stickbreaking_dirichlet(self.eta, lamb, beta).sum()  
+                
+class LdaJointDistributionWithSphereDir:
+    
+    def __init__(self, x, alpha, eta):
+        self.x = x 
+        self.alpha = alpha
+        self.eta = eta
+        
+    def unnormalized_log_prob(self, params):
+        theta = params['theta']
+        pi = theta ** 2.
+        lamb = params['lamb']
+        beta = torch.distributions.StickBreakingTransform()(lamb)
+        avg = torch.matmul(pi, beta)
+        return dist.Multinomial(probs = avg).log_prob(self.x).sum() \
+                + unnormalized_log_prob_spherical_dirichlet(self.alpha, theta).sum() \
+                + unnormalized_log_prob_spherical_dirichlet(self.eta, lamb).sum()  
                 
